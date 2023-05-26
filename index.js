b7c405ca42ef3808158bd0acebec9c23f752925a
haltim <timoayomide2000@gmail.com>
2023-05-27 00:37:36 +0100

    changes

--- index.js
+++ index.js
@@ -1,77 +1,28 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const InitiateMongoServer = require("./config/db");
+const user = require("./router/user");
 const cors = require("cors");
-const mongoose = require("mongoose");
-const bodyParser = require('body-parser')
-require("dotenv").config();
 
-const app = express()
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(cors())
+InitiateMongoServer();
 
-mongoose
-  .connect(process.env.MONGO_URL, {
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("DB Connection Successfull");
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+const app = express();
+app.use(cors());
 
-const userSchema = new mongoose.Schema({
-  username: String,
-  email: String,
-  password: String
-})
-const User = new mongoose.model("User", userSchema)
-// Route
+// middleware
+app.use(bodyParser.json());
+
+// PORT
+const PORT = 4000;
 
 app.get("/", (req, res) => {
-  res.send("My app")
-})
-// post 
-app.post("/login", (req, res) => {
-  const { email, password } = req.body
-  User.findOne({ email: email }, (err, user) => {
-    if (user) {
-      if (password === user.password) {
-        res.send({ massage: "login successfull", user: user })
-      } else {
-        res.send({ massage: "password did't match" })
-      }
+  res.json({ message: "API Working" });
+});
 
-    } else {
-      res.send({ massage: "User not registered" })
-    }
-  })
+// router
 
-})
+app.use("/user", user);
 
-app.post("/register", (req, res) => {
-  const { username, email, password } = req.body
-  User.findOne({ email: email }, (err, user) => {
-    if (user) {
-      res.send({ massage: "Already Registerd" })
-    } else {
-      const user = new User({
-        username,
-        email,
-        password
-      })
-      user.save(err => {
-        if (err) {
-          res.send(err)
-        } else {
-          res.send({ message: "Successfully Registered ->Please login!!" })
-        }
-      })
-    }
-  })
-
-})
-
-app.listen(8000, () => {
-  console.log("DB successfully started on port 8000")
-})
+app.listen(PORT, (req, res) => {
+  console.log(`Server Started at PORT ${PORT}`);
+});
